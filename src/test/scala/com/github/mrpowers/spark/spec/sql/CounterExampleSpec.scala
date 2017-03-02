9178da161cb5ebcb5b0c3809a117f477358a7a53
Anh Nguyen <anguyen@medivo.com>
2017-03-02 11:05:28 -0500

    Add an example to show an issue with spark-testing-base library

---
+++ src/test/scala/com/github/mrpowers/spark/spec/sql/CounterExampleSpec.scala
@@ -0,0 +1,47 @@
+package com.github.mrpowers.spark.spec.sql
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.scalatest._
+import org.apache.spark.sql.functions._
+
+class CounterExampleSpec extends FunSpec with ShouldMatchers with DataFrameSuiteBase {
+
+  import spark.implicits._
+
+  describe("#spark-testing-base-issue-1") {
+
+    it("fails to compare dataframes with same content") {
+
+      // Generally speaking, in a dataframe the order of rows has no meaning,
+      //   like they are in a sql table
+      // But this counter example shows that spark-testing-base doesn't accept that
+      // In a spark job, final dataframe is created via multiple transformation
+      //    and the rows order is not preserved.
+      // So spark-testing-base may not be suitable for testing complicated transformation.
+
+      val firstDf = Seq(
+        ("TOM","CAT","1234567890"),
+        ("JERRY","MOUSE","1324567890")
+      ).toDF("first_name","last_name","phone")
+
+      // secondDf has same content, but rows are swapped
+      val secondDf = Seq(
+        ("JERRY","MOUSE","1324567890"),
+        ("TOM","CAT","1234567890")
+      ).toDF("first_name","last_name","phone")
+
+      var good = 0
+      try {
+        // spark-testing-base fails here
+        assertDataFrameEquals(firstDf, secondDf)
+      } catch {
+        // we catch the error and claim the test passed
+        case _: Throwable => good = 1
+      }
+      assert(good == 1)
+
+    }
+
+  }
+
+}

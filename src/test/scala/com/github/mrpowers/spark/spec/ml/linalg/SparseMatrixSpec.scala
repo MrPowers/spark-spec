aee5312a9e9dc4c48dff51b0d70ae21566ccda12
Brian Saindon <bsaindon@medivo.com>
2017-03-13 13:29:46 -0400

    added sparse to dense matrix transform

--- src/test/scala/com/github/mrpowers/spark/spec/ml/linalg/SparseMatrixSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/spec/ml/linalg/SparseMatrixSpec.scala
@@ -1,11 +1,9 @@
 package com.github.mrpowers.spark.spec.ml.linalg
 
-import org.apache.spark.ml.linalg.SparseMatrix
+import org.apache.spark.ml.linalg.{SparseMatrix}
 import org.scalatest.FunSpec
 
-/**
-  * Created by BrianS on 3/7/17.
-  */
+
 class SparseMatrixSpec extends FunSpec  {
 
   describe("new") {
@@ -16,6 +14,17 @@ class SparseMatrixSpec extends FunSpec  {
     assert(s.getClass().getName() === "org.apache.spark.ml.linalg.SparseMatrix")
 
     }
+
+  }
+
+  describe("creates a dense matrix from a given sparse matrix") {
+
+    it ("creates a Dense Matrix from a Sparse Matrix") {
+
+    val s = new SparseMatrix(3, 3, Array(0, 2, 3, 6), Array(0, 2, 1, 0, 1, 2), Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
+    val d = s.toDense
+    assert(d.getClass().getName() ==="org.apache.spark.ml.linalg.DenseMatrix")
+     }
 
   }
 

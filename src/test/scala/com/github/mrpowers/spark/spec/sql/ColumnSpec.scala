64099fc90b298eb2796f7507c64d7d990eb85bb5
MrPowers <matthewkevinpowers@gmail.com>
2017-03-16 18:52:20 -0500

    Alphebetize Column methods

--- src/test/scala/com/github/mrpowers/spark/spec/sql/ColumnSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/spec/sql/ColumnSpec.scala
@@ -32,214 +32,31 @@ class ColumnSpec extends FunSpec with DataFrameSuiteBase {
 
   }
 
+  describe("#endsWith") {
 
-  describe("#geq") {
-
-    it("matches for greater than or equal to an expression") {
+    it("string ends with") {
 
       val sourceDf = Seq(
-        ("1980-09-10"),
-        ("1990-04-18"),
-        ("2000-04-18"),
-        ("2010-04-18"),
-        ("2016-01-10")
-      ).toDF("some_date")
-      .withColumn("some_date", $"some_date".cast("timestamp"))
+        ("A","1970"),
+        ("B","1986"),
+        ("C","1990"),
+        ("D","2000"),
+        ("E","2012"))
+        .toDF("id", "year")
 
-      val actualDf = sourceDf.where(col("some_date").geq("1995-04-18"))
+
+      val actualDf = sourceDf.where(col("year").endsWith("0"))
 
       val expectedDf = Seq(
-        ("2000-04-18"),
-        ("2010-04-18"),
-        ("2016-01-10")
-      ).toDF("some_date")
-      .withColumn("some_date", $"some_date".cast("timestamp"))
+        ("A","1970"),
+        ("C","1990"),
+        ("D","2000"))
+        .toDF("id", "year")
 
       assertDataFrameEquals(actualDf, expectedDf)
 
     }
-    
-  }
 
-  describe("#gt") { 
-    
-    it("keeps rows greater than a certain number") { 
-      
-      val sourceDf = Seq(
-        (45),
-        (79),
-        (124),
-        (196),
-        (257)
-      ).toDF("num1")
-      
-      val actualDf = sourceDf.where(col("num1").gt(124))
-      
-      val expectedDf = Seq(
-        (196),
-        (257)
-      ).toDF("num1")
-      
-      assertDataFrameEquals(actualDf, expectedDf)
-      
-    }
-    
-  }
-  
-  describe("#minus") {
-    
-    it("Subtraction. Subtract the other expression from this expression.") {
-      
-      val sourceDf = Seq(
-        (45),
-        (79),
-        (55),
-        (124)
-      ).toDF("num1")
-      
-      val actualDf = sourceDf.select(
-        sourceDf.col("num1"),
-        sourceDf.col("num1").minus(55).as("num2")
-      )
-      
-      val expectedData = List(
-        Row(45, -10),
-        Row(79, 24),
-        Row(55, 0),
-        Row(124, 69)
-      )
-
-      val expectedSchema = List(
-        StructField("num1", IntegerType, false),
-        StructField("num2", IntegerType, false)
-      )
-
-      val expectedDf = spark.createDataFrame(
-        spark.sparkContext.parallelize(expectedData),
-        StructType(expectedSchema)
-      )
-      
-      assertDataFrameEquals(actualDf, expectedDf)
-           
-    }
-    
-  }
-  
-  describe("#multiply") {
-    
-    it("Multiplication of this expression and another expression.") {
-      
-      val sourceDf = Seq(
-        (7),
-        (14),
-        (23),
-        (41)
-      ).toDF("num1")
-      
-      val actualDf = sourceDf.select(
-        sourceDf.col("num1"),
-        sourceDf.col("num1").multiply(5).as("num2")
-      )
-      
-      val expectedData = List(
-        Row(7, 35),
-        Row(14, 70),
-        Row(23, 115),
-        Row(41, 205)
-      )
-
-      val expectedSchema = List(
-        StructField("num1", IntegerType, false),
-        StructField("num2", IntegerType, false)
-      )
-
-      val expectedDf = spark.createDataFrame(
-        spark.sparkContext.parallelize(expectedData),
-        StructType(expectedSchema)
-      )
-      
-      assertDataFrameEquals(actualDf, expectedDf)
-           
-    }
-    
-  }
- 
-  describe("#plus") {
-    
-    it("sum of this expression and another expression") {
-      
-      val sourceDf = Seq(
-        (45),
-        (79),
-        (0),
-        (124)
-      ).toDF("num1")
-      
-      val actualDf = sourceDf.select(
-        sourceDf.col("num1"),
-        sourceDf.col("num1").plus(55).as("num2")
-      )
-      
-      val expectedData = List(
-        Row(45, 100),
-        Row(79, 134),
-        Row(0, 55),
-        Row(124, 179)
-      )
-
-      val expectedSchema = List(
-        StructField("num1", IntegerType, false),
-        StructField("num2", IntegerType, false)
-      )
-
-      val expectedDf = spark.createDataFrame(
-        spark.sparkContext.parallelize(expectedData),
-        StructType(expectedSchema)
-      )
-      
-      assertDataFrameEquals(actualDf, expectedDf)
-           
-    }
-    
-  }
-  
-  describe("#substr") {
-    
-    it("An expression that returns a substring. substr(startPos: Int, len: Int): Column") {
-      
-      val sourceDf = Seq(
-        ("mayflower"),
-        ("nightshift"),
-        (null),
-        ("lamplight")
-      ).toDF("word")
-      
-      val actualDf = sourceDf.select(
-        sourceDf.col("word"),
-        sourceDf.col("word").substr(4, 3).as("word_part")
-      )
-      
-      val expectedData = List(
-        Row("mayflower", "flo"),
-        Row("nightshift", "hts"),
-        Row(null, null),
-        Row("lamplight", "pli")
-      )
-
-      val expectedSchema = List(
-        StructField("word", StringType, true),
-        StructField("word_part", StringType, true)
-      )
-
-      val expectedDf = spark.createDataFrame(
-        spark.sparkContext.parallelize(expectedData),
-        StructType(expectedSchema)
-      )
-      
-      assertDataFrameEquals(actualDf, expectedDf)
-           
-    }
-    
   }
 
   describe("#isin") {
@@ -268,32 +85,6 @@ class ColumnSpec extends FunSpec with DataFrameSuiteBase {
 
   }
 
-  describe("#endsWith") {
-
-    it("string ends with") {
-
-      val sourceDf = Seq(
-          ("A","1970"),
-          ("B","1986"),
-          ("C","1990"),
-          ("D","2000"),
-          ("E","2012"))
-        .toDF("id", "year")
-
-
-      val actualDf = sourceDf.where(col("year").endsWith("0"))
-
-      val expectedDf = Seq(
-        ("A","1970"),
-        ("C","1990"),
-        ("D","2000"))
-        .toDF("id", "year")
-
-      assertDataFrameEquals(actualDf, expectedDf)
-
-    }
-
-  }
 
   describe("#getItem") {
 
@@ -324,22 +115,94 @@ class ColumnSpec extends FunSpec with DataFrameSuiteBase {
 
   }
 
-  describe("#rlike") {
-    it("pattern match of a string pattern against pattern") {
+
+  describe("#geq") {
+
+    it("matches for greater than or equal to an expression") {
 
       val sourceDf = Seq(
-        ("A", "bbbbba11111xy"),
-        ("B", "aaaab11111xy"),
-        ("C", "aaaab22111xy"))
-        .toDF("id", "text")
+        ("1980-09-10"),
+        ("1990-04-18"),
+        ("2000-04-18"),
+        ("2010-04-18"),
+        ("2016-01-10")
+      ).toDF("some_date")
+      .withColumn("some_date", $"some_date".cast("timestamp"))
 
-    val actualDf = sourceDf.filter($"text".rlike("b{5}.[1]{5}(x|y){2}"))
+      val actualDf = sourceDf.where(col("some_date").geq("1995-04-18"))
 
-    val expectedDf = Seq(
-      ("A", "bbbbba11111xy"))
-      .toDF("id", "text")
+      val expectedDf = Seq(
+        ("2000-04-18"),
+        ("2010-04-18"),
+        ("2016-01-10")
+      ).toDF("some_date")
+      .withColumn("some_date", $"some_date".cast("timestamp"))
 
-    assertDataFrameEquals(actualDf, expectedDf)
+      assertDataFrameEquals(actualDf, expectedDf)
+
+    }
+
+  }
+
+  describe("#gt") {
+
+    it("keeps rows greater than a certain number") {
+
+      val sourceDf = Seq(
+        (45),
+        (79),
+        (124),
+        (196),
+        (257)
+      ).toDF("num1")
+
+      val actualDf = sourceDf.where(col("num1").gt(124))
+
+      val expectedDf = Seq(
+        (196),
+        (257)
+      ).toDF("num1")
+
+      assertDataFrameEquals(actualDf, expectedDf)
+
+    }
+
+  }
+
+  describe("#minus") {
+
+    it("Subtraction. Subtract the other expression from this expression.") {
+
+      val sourceDf = Seq(
+        (45),
+        (79),
+        (55),
+        (124)
+      ).toDF("num1")
+
+      val actualDf = sourceDf.select(
+        sourceDf.col("num1"),
+        sourceDf.col("num1").minus(55).as("num2")
+      )
+
+      val expectedData = List(
+        Row(45, -10),
+        Row(79, 24),
+        Row(55, 0),
+        Row(124, 69)
+      )
+
+      val expectedSchema = List(
+        StructField("num1", IntegerType, false),
+        StructField("num2", IntegerType, false)
+      )
+
+      val expectedDf = spark.createDataFrame(
+        spark.sparkContext.parallelize(expectedData),
+        StructType(expectedSchema)
+      )
+
+      assertDataFrameEquals(actualDf, expectedDf)
 
     }
 
@@ -366,6 +229,144 @@ class ColumnSpec extends FunSpec with DataFrameSuiteBase {
         ("D", "2014"),
         ("F", "2016"))
         .toDF("id", "year")
+
+      assertDataFrameEquals(actualDf, expectedDf)
+
+    }
+
+  }
+
+  describe("#multiply") {
+
+    it("Multiplication of this expression and another expression.") {
+
+      val sourceDf = Seq(
+        (7),
+        (14),
+        (23),
+        (41)
+      ).toDF("num1")
+
+      val actualDf = sourceDf.select(
+        sourceDf.col("num1"),
+        sourceDf.col("num1").multiply(5).as("num2")
+      )
+
+      val expectedData = List(
+        Row(7, 35),
+        Row(14, 70),
+        Row(23, 115),
+        Row(41, 205)
+      )
+
+      val expectedSchema = List(
+        StructField("num1", IntegerType, false),
+        StructField("num2", IntegerType, false)
+      )
+
+      val expectedDf = spark.createDataFrame(
+        spark.sparkContext.parallelize(expectedData),
+        StructType(expectedSchema)
+      )
+
+      assertDataFrameEquals(actualDf, expectedDf)
+
+    }
+
+  }
+
+  describe("#plus") {
+
+    it("sum of this expression and another expression") {
+
+      val sourceDf = Seq(
+        (45),
+        (79),
+        (0),
+        (124)
+      ).toDF("num1")
+
+      val actualDf = sourceDf.select(
+        sourceDf.col("num1"),
+        sourceDf.col("num1").plus(55).as("num2")
+      )
+
+      val expectedData = List(
+        Row(45, 100),
+        Row(79, 134),
+        Row(0, 55),
+        Row(124, 179)
+      )
+
+      val expectedSchema = List(
+        StructField("num1", IntegerType, false),
+        StructField("num2", IntegerType, false)
+      )
+
+      val expectedDf = spark.createDataFrame(
+        spark.sparkContext.parallelize(expectedData),
+        StructType(expectedSchema)
+      )
+
+      assertDataFrameEquals(actualDf, expectedDf)
+
+    }
+
+  }
+
+  describe("#rlike") {
+    it("pattern match of a string pattern against pattern") {
+
+      val sourceDf = Seq(
+        ("A", "bbbbba11111xy"),
+        ("B", "aaaab11111xy"),
+        ("C", "aaaab22111xy"))
+        .toDF("id", "text")
+
+      val actualDf = sourceDf.filter($"text".rlike("b{5}.[1]{5}(x|y){2}"))
+
+      val expectedDf = Seq(
+        ("A", "bbbbba11111xy"))
+        .toDF("id", "text")
+
+      assertDataFrameEquals(actualDf, expectedDf)
+
+    }
+
+  }
+
+  describe("#substr") {
+
+    it("An expression that returns a substring. substr(startPos: Int, len: Int): Column") {
+
+      val sourceDf = Seq(
+        ("mayflower"),
+        ("nightshift"),
+        (null),
+        ("lamplight")
+      ).toDF("word")
+
+      val actualDf = sourceDf.select(
+        sourceDf.col("word"),
+        sourceDf.col("word").substr(4, 3).as("word_part")
+      )
+
+      val expectedData = List(
+        Row("mayflower", "flo"),
+        Row("nightshift", "hts"),
+        Row(null, null),
+        Row("lamplight", "pli")
+      )
+
+      val expectedSchema = List(
+        StructField("word", StringType, true),
+        StructField("word_part", StringType, true)
+      )
+
+      val expectedDf = spark.createDataFrame(
+        spark.sparkContext.parallelize(expectedData),
+        StructType(expectedSchema)
+      )
 
       assertDataFrameEquals(actualDf, expectedDf)
 

95a1efa7a4f5cee77d6618fa2a28299478ec7ee6
MrPowers <matthewkevinpowers@gmail.com>
2017-03-22 15:52:49 -0500

    Alphabetize the DenseMatrixSpec file

--- src/test/scala/com/github/mrpowers/spark/spec/ml/linalg/DenseMatrixSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/spec/ml/linalg/DenseMatrixSpec.scala
@@ -18,13 +18,13 @@ class DenseMatrixSpec extends FunSpec {
 
   }
 
-  describe("#transpose") {
+  describe("#apply") {
 
-    it("transposes the matrix") {
+    it("gets the (i,j)th element of a matrix") {
 
-      val m = new DenseMatrix(3, 2, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0))
-      val actual = m.transpose
-      val expected = new DenseMatrix(2, 3, Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
+      val m = new DenseMatrix(4, 3, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0))
+      val actual = m.apply(2, 2)
+      val expected = 11.0
       assert(actual === expected)
 
     }
@@ -44,14 +44,38 @@ class DenseMatrixSpec extends FunSpec {
 
   }
 
-  describe("#apply") {
+  describe("#multiply") {
 
-    it("gets the (i,j)th element of a matrix") {
+    it("multiplies two dense matrices") {
 
-      val m = new DenseMatrix(4, 3, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0))
-      val actual = m.apply(2, 2)
-      val expected = 11.0
+      val m = new DenseMatrix(2, 2, Array(1.0, 2.0, 3.0, 4.0))
+      val n = new DenseMatrix(2, 1, Array(1.0, 10.0))
+
+      val actual = m.multiply(n)
+      val expected = new DenseMatrix(2, 1, Array(31.0, 42.0))
       assert(actual === expected)
+
+    }
+
+    it("multiplies a matrix with a vector") {
+
+      val a = new DenseMatrix(2, 2, Array(1.0, 2.0, 3.0, 4.0))
+      val b = new DenseVector(Array(1.0, 100.0))
+
+      val actual = a.multiply(b)
+      val expected = new DenseVector(Array(301.0, 402.0))
+      assert(actual === expected)
+
+    }
+
+  }
+
+  describe("#numActives") {
+
+    it("gets the number of values stored explicitly") {
+
+      val m = new DenseMatrix(4, 3, Array(0.0, 3.0, 5.0, 2.0, 4.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0))
+      assert(m.numActives === 12)
 
     }
 
@@ -79,37 +103,13 @@ class DenseMatrixSpec extends FunSpec {
 
   }
 
-  describe("#numActives") {
+  describe("#transpose") {
 
-    it("gets the number of values stored explicitly") {
+    it("transposes the matrix") {
 
-      val m = new DenseMatrix(4, 3, Array(0.0, 3.0, 5.0, 2.0, 4.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0))
-      assert(m.numActives === 12)
-
-    }
-
-  }
-
-  describe("#multiply") {
-
-    it("multiplies two dense matrices") {
-
-      val m = new DenseMatrix(2, 2, Array(1.0, 2.0, 3.0, 4.0))
-      val n = new DenseMatrix(2, 1, Array(1.0, 10.0))
-
-      val actual = m.multiply(n)
-      val expected = new DenseMatrix(2, 1, Array(31.0, 42.0))
-      assert(actual === expected)
-
-    }
-
-    it("multiplies a matrix with a vector") {
-
-      val a = new DenseMatrix(2, 2, Array(1.0, 2.0, 3.0, 4.0))
-      val b = new DenseVector(Array(1.0, 100.0))
-
-      val actual = a.multiply(b)
-      val expected = new DenseVector(Array(301.0, 402.0))
+      val m = new DenseMatrix(3, 2, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0))
+      val actual = m.transpose
+      val expected = new DenseMatrix(2, 3, Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
       assert(actual === expected)
 
     }

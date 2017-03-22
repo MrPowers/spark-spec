6a494407eda9b17d6a2489a98989bba867b67083
MrPowers <matthewkevinpowers@gmail.com>
2017-03-22 15:33:12 -0500

    Format the SparseMatrixSpec whitespace

--- src/test/scala/com/github/mrpowers/spark/spec/ml/linalg/SparseMatrixSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/spec/ml/linalg/SparseMatrixSpec.scala
@@ -3,15 +3,14 @@ package com.github.mrpowers.spark.spec.ml.linalg
 import org.apache.spark.ml.linalg.{SparseMatrix}
 import org.scalatest.FunSpec
 
-
-class SparseMatrixSpec extends FunSpec  {
+class SparseMatrixSpec extends FunSpec {
 
   describe("new") {
 
     it("creates a SparseMatrix") {
 
-    val s = new SparseMatrix(3, 3, Array(0, 2, 3, 6), Array(0, 2, 1, 0, 1, 2), Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
-    assert(s.getClass().getName() === "org.apache.spark.ml.linalg.SparseMatrix")
+      val s = new SparseMatrix(3, 3, Array(0, 2, 3, 6), Array(0, 2, 1, 0, 1, 2), Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
+      assert(s.getClass().getName() === "org.apache.spark.ml.linalg.SparseMatrix")
 
     }
 
@@ -19,12 +18,12 @@ class SparseMatrixSpec extends FunSpec  {
 
   describe("creates a dense matrix from a given sparse matrix") {
 
-    it ("creates a Dense Matrix from a Sparse Matrix") {
+    it("creates a Dense Matrix from a Sparse Matrix") {
 
-    val s = new SparseMatrix(3, 3, Array(0, 2, 3, 6), Array(0, 2, 1, 0, 1, 2), Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
-    val d = s.toDense
-    assert(d.getClass().getName() ==="org.apache.spark.ml.linalg.DenseMatrix")
-     }
+      val s = new SparseMatrix(3, 3, Array(0, 2, 3, 6), Array(0, 2, 1, 0, 1, 2), Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
+      val d = s.toDense
+      assert(d.getClass().getName() === "org.apache.spark.ml.linalg.DenseMatrix")
+    }
 
   }
 

8345633faf7091097b020bb911dc52e04b277f6f
Brian Saindon <bsaindon@medivo.com>
2017-03-24 16:55:02 -0400

    added pending methods to Binarizer Spec

---
+++ src/test/scala/com/github/mrpowers/spark/spec/ml/feature/BinarizerSpec.scala
@@ -0,0 +1,132 @@
+package com.github.mrpowers.spark.spec.ml.feature
+
+import org.scalatest.FunSpec
+
+
+class BinarizerSpec extends FunSpec{
+
+  describe("#clear") {
+    pending
+  }
+
+  describe ("#copy") {
+    pending
+  }
+
+  describe ("#explainParam") {
+    pending
+  }
+
+  describe ("#explainParams") {
+    pending
+  }
+
+  describe ("#extractParamMap") {
+    pending
+  }
+
+  describe ("#get") {
+    pending
+  }
+
+  describe ("#getDefault") {
+    pending
+  }
+
+  describe ("#getInputCol") {
+    pending
+  }
+
+  describe ("#getOrDefault") {
+    pending
+  }
+
+  describe ("#getOutputCol") {
+    pending
+  }
+
+  describe ("#getParam") {
+    pending
+  }
+
+  describe ("#getThereshold") {
+    pending
+  }
+
+  describe ("#hasDefault") {
+    pending
+  }
+
+  describe ("#hasParam") {
+    pending
+  }
+
+  describe ("#inputCol") {
+    pending
+  }
+
+  describe ("#isDefined") {
+    pending
+  }
+
+  describe ("#isSet") {
+    pending
+  }
+
+  describe ("#load") {
+    pending
+  }
+
+  describe ("#outputCol") {
+    pending
+  }
+
+  describe ("#params") {
+    pending
+  }
+
+  describe ("#save") {
+    pending
+  }
+
+  describe ("#set") {
+    pending
+  }
+
+  describe ("#setInputCol") {
+    pending
+  }
+
+  describe ("#setOutputCol") {
+    pending
+  }
+
+  describe ("#setThreshold") {
+    pending
+  }
+
+  describe ("#threshold") {
+    pending
+  }
+
+  describe ("#toString") {
+    pending
+  }
+
+  describe ("#transform") {
+    pending
+  }
+
+  describe ("#transformSchema") {
+    pending
+  }
+
+  describe ("#uid") {
+    pending
+  }
+
+  describe ("#write") {
+    pending
+  }
+
+}

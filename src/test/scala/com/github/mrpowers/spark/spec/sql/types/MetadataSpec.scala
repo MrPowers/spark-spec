105376e38508648a33742f25f1acc0c898e8ac6c
MrPowers <matthewkevinpowers@gmail.com>
2017-03-25 16:59:01 -0500

    Add a skeleton for Metadata specs

---
+++ src/test/scala/com/github/mrpowers/spark/spec/sql/types/MetadataSpec.scala
@@ -0,0 +1,67 @@
+package com.github.mrpowers.spark.spec.sql.types
+
+import org.scalatest.FunSpec
+
+class MetadataSpec extends FunSpec {
+
+  describe("#contains") {
+    pending
+  }
+
+  describe("#equals") {
+    pending
+  }
+
+  describe("#getBoolean") {
+    pending
+  }
+
+  describe("#getBooleanArray") {
+    pending
+  }
+
+  describe("#getDouble") {
+    pending
+  }
+
+  describe("#getDoubleArray") {
+    pending
+  }
+
+  describe("#getLong") {
+    pending
+  }
+
+  describe("#getLongArray") {
+    pending
+  }
+
+  describe("#getMetadata") {
+    pending
+  }
+
+  describe("#getMetadataArray") {
+    pending
+  }
+
+  describe("#getString") {
+    pending
+  }
+
+  describe("#getStringArray") {
+    pending
+  }
+
+  describe("#hastCode") {
+    pending
+  }
+
+  describe("#json") {
+    pending
+  }
+
+  describe("#toString") {
+    pending
+  }
+
+}

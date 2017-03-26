ff45cfcd776a7f850b7449083d6a70a721fb6796
MrPowers <matthewkevinpowers@gmail.com>
2017-03-26 08:56:05 -0500

    Add some more metadata specs

--- src/test/scala/com/github/mrpowers/spark/spec/sql/types/MetadataSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/spec/sql/types/MetadataSpec.scala
@@ -1,15 +1,38 @@
 package com.github.mrpowers.spark.spec.sql.types
 
+import org.apache.spark.sql.types.MetadataBuilder
 import org.scalatest.FunSpec
 
 class MetadataSpec extends FunSpec {
 
   describe("#contains") {
-    pending
+
+    it("returns true if the Metadata object contains a key") {
+
+      val m = new MetadataBuilder().putString("comment", "crazy").build()
+      assert(m.contains("comment"), true)
+
+    }
+
+    it("returns false if the Metadata object doesn't contain a key") {
+
+      val m = new MetadataBuilder().putString("comment", "crazy").build()
+      assert(m.contains("bulbasaur"), false)
+
+    }
+
   }
 
   describe("#equals") {
-    pending
+
+    it("returns true if another Metadata object is the same") {
+
+      val m1 = new MetadataBuilder().putString("comment", "crazy").build()
+      val m2 = new MetadataBuilder().putString("comment", "crazy").build()
+      assert(m1.equals(m2) === true)
+
+    }
+
   }
 
   describe("#getBoolean") {
